4821624eaf88ecb6472cf6ad637beaba41064dde
Vlad <vlivonika@gmail.com>
2024-04-23 13:38:44 +0200

    add slider in section top

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-
-  loop: true,
+  effect: "fade",
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
+  // loop: true,
 
   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
 
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
   },
 });
