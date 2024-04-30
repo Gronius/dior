f9a10ebbed4f8f97268736ea725e4275aafa95a2
Vlad <vlivonika@gmail.com>
2024-04-30 08:09:23 +0200

    add footer

--- js/main.js
+++ js/main.js
@@ -23,9 +23,15 @@ const swiperAbout = new Swiper(".about__slider", {
   slidesPerView: 4,
   spaceBetween: 20,
   freeMode: true,
-  // loop: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+document.querySelectorAll(".accordeon__triger").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentNode.classList.toggle("accordeon__item--active")
+  })
+} )
